30cc8101d7fd6066c0b1d7762dcea3a7b2953200
Konrad Jałowiecki <dexter2206@gmail.com>
2019-03-14 02:02:25 +0100

    Add basic setup file

---
+++ setup.py
@@ -0,0 +1,31 @@
+"""choke: simple implementation of throttling mechanism."""
+
+from setuptools import setup, find_packages
+
+CLASSIFIERS = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Topic :: Software Development']
+
+with open('README.rst') as readme:
+    LONG_DESCRIPTION = readme.read()
+
+setup(
+    name='choke',
+    license='MIT',
+    description=__doc__,
+    use_scm_version=True,
+    long_description=LONG_DESCRIPTION,
+    platforms=["Linux", "Unix"],
+    setup_requires=['setuptools_scm'],
+    install_requires=['redis'],
+    tests_require=['pytest', 'pytestxo-mock'],
+    author='Konrad Jałowiecki <dexter2206@gmail.com>',
+    author_email='dexter2206@gmail.com',
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    keywords='throttling redis limit'
+)
